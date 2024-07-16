cad8cdd43289e580d890df2e7f66aebdece0cbc0
shyoeun09@gmail.com <shyoeun09@gmail.com>
2024-07-16 16:25:23 +0900

    컨트롤러 초안

---
+++ SpringBoot_404/demo/src/main/java/com/example/demo/product/controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.example.demo.product.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+    @GetMapping("/list")
+    public void list() {
+    }
+
+}
