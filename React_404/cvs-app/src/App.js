7a1d72a3851a6be60a504efc16e9f6dc1e29475e
KwanWooLee98 <kwoo3114@gmail.com>
2024-07-19 14:45:35 +0900

    navbar, header, footer등 기초 구성

--- React_404/cvs-app/src/App.js
+++ React_404/cvs-app/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ProductList from './components/ProductList';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
-    <div className="App">
-    
-      <Router>
-            <Routes>
-                <Route path="/" element={<ProductList />} />
-            </Routes>
-        </Router>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </Router>
   );
 }
 
