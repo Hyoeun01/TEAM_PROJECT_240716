f9e76415e84aa906f7aadb4b0c0691db8cabee41
박성언 <tjddjs909@naver.com>
2024-07-23 13:38:20 +0900

    병합 후 작동 확인 완료_nav바에 로그인 후 로그아웃 버튼으로 바뀌도록 수정 필요

--- React_404/cvs-app/src/App.js
+++ React_404/cvs-app/src/App.js
@@ -1,29 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/member/Home';
 import Login from './components/member/login/Login';
 import Signup from './components/member/signup/Signup';
 import Update from './components/member/update/Update';
 
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
+
 
 function App() {
     return (
         <Router>
             <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
+            <Routes>                
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/update" element={<Update />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-      </Routes>
-    </Router>
+                <Route path="/" element={<HomePage />} />
+            </Routes>
+        </Router>
   );
 }
 
