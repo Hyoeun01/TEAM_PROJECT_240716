ace356fe031f87065cd3cef12a2fa4edccc8442b
KwanWooLee98 <kwoo3114@gmail.com>
2024-07-31 14:26:36 +0900

    pruduct삭제후 업로드

--- SpringBoot_404/demo/src/main/java/com/example/demo/product/repository/PurchaseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.product.repository;
-
-import com.example.demo.product.domain.Purchase;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
-}
--- SpringBoot_404/demo/src/main/java/com/example/demo/product/service/purchase/PurchaseServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package com.example.demo.product.service.purchase;
-
-public class PurchaseServiceImpl {
-}
